05e804a63bf009986e6f71a4186416308d27bbfc
Alexander Haralampiev <sasho2004@gmail.com>
2024-05-16 22:36:58 +0300

    Update settings.py

--- etl_news_project/spider/new_spiders.py/settings.py
+++ etl_news_project/spider/new_spiders.py/settings.py
@@ -1,3 +1,5 @@
+# settings.py
+
 # Scrapy settings for news_spiders project
 BOT_NAME = 'news_spiders'
 
@@ -10,4 +12,9 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 ITEM_PIPELINES = {
     'news_spiders.pipelines.NewsSpidersPipeline': 300,
+}
+
+# Configure custom middlewares
+DOWNLOADER_MIDDLEWARES = {
+    'news_spiders.middlewares.SimpleLoggingMiddleware': 543,
 }
