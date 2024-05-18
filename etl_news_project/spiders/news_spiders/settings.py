d366cead0f82c7f9b1acf510654df9b66757a7f9
Alexander Haralampiev <sasho2004@gmail.com>
2024-05-19 00:01:44 +0300

    getting there
    DAY 3 12:00 night

--- etl_news_project/spiders/news_spiders/settings.py
+++ etl_news_project/spiders/news_spiders/settings.py
@@ -1,14 +1,53 @@
-# news_spiders/settings.py
+BOT_NAME = 'etl_news_project'
 
-BOT_NAME = 'news_spiders'
+SPIDER_MODULES = ['etl_news_project.spiders.news_spiders.spiders']
+NEWSPIDER_MODULE = 'etl_news_project.spiders.news_spiders.spiders'
 
-SPIDER_MODULES = ['news_spiders.spiders']
-NEWSPIDER_MODULE = 'news_spiders.spiders'
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 16
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Configure a delay for requests for the same website (default: 0)
+DOWNLOAD_DELAY = 2
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Language': 'en',
+}
+
+# Enable or disable spider middlewares
+SPIDER_MIDDLEWARES = {
+   'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+# Enable or disable downloader middlewares
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
+
+# Enable or disable extensions
+EXTENSIONS = {
+   'scrapy.extensions.telnet.TelnetConsole': None,
+}
 
 # Configure item pipelines
 ITEM_PIPELINES = {
-    'news_spiders.pipelines.NewsSpidersPipeline': 300,
+   'etl_news_project.spiders.news_spiders.pipelines.NewsSpidersPipeline': 300,
 }
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_DEBUG = False
+
+# Disable HTTP caching as it may interfere with Selenium
+HTTPCACHE_ENABLED = False
--- etl_news_project/spiders/news_spiders/spiders/restofworld_spider.py
+++ etl_news_project/spiders/news_spiders/spiders/restofworld_spider.py
@@ -1,6 +1,12 @@
 import scrapy
+from scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from ..items import ArticleItem
 from etl.transform_data import clean_html, extract_entities
+from datetime import datetime
 
 class RestOfWorldSpider(scrapy.Spider):
     name = 'restofworld_spider'
@@ -8,12 +14,26 @@ class RestOfWorldSpider(scrapy.Spider):
     article_count = 0
     max_articles = 20
 
+    def __init__(self, *args, **kwargs):
+        super(RestOfWorldSpider, self).__init__(*args, **kwargs)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
     def parse(self, response):
+        self.driver.get(response.url)
         self.logger.info(f"Crawling URL: {response.url} with status {response.status}")
 
-        # Check for different structures of article links
-        article_links = response.css('a.grid-story__link.article-link::attr(href)').getall()
-        article_links += response.css('a.article-link::attr(href)').getall()
+        # Scroll to load dynamic content
+        self.scroll_to_load()
+
+        # Get the page source and create a Selector object
+        sel = Selector(text=self.driver.page_source)
+
+        article_links = sel.css('a.grid-story__link.article-link::attr(href)').getall()
+        article_links += sel.css('a.article-link::attr(href)').getall()
         
         self.logger.info(f"Found {len(article_links)} article links")
 
@@ -24,33 +44,49 @@ class RestOfWorldSpider(scrapy.Spider):
             else:
                 return
 
-        # Follow pagination links
-        next_page = response.css('a.o-card__more::attr(href)').get()
+        next_page = sel.css('a.o-card__more::attr(href)').get()
         if not next_page:
-            next_page = response.css('a.pagination__next::attr(href)').get()
+            next_page = sel.css('a.pagination__next::attr(href)').get()
 
         if next_page and self.article_count < self.max_articles:
             self.logger.info(f"Found next page link: {next_page}")
-            yield response.follow(next_page, self.parse)
+            yield scrapy.Request(next_page, self.parse)
 
     def parse_article(self, response):
+        self.driver.get(response.url)
         self.logger.info(f"Scraping article URL: {response.url} with status {response.status}")
+
+        # Get the page source and create a Selector object
+        sel = Selector(text=self.driver.page_source)
 
         item = ArticleItem()
 
-        item['title'] = response.css('h1.post-header__text__title::text').get() or \
-                        response.css('h1.grid-story__hed.headline::text').get() or \
-                        response.css('h2.article-info__title.headline::text').get()
+        item['title'] = sel.css('h1.post-header__text__title::text').get() or \
+                        sel.css('h1.grid-story__hed.headline::text').get() or \
+                        sel.css('h2.article-info__title.headline::text').get()
         item['url'] = response.url
-        item['body'] = ' '.join(response.css('div.post-container p::text').getall())
-        item['pub_datetime'] = response.css('div.contrib-date time::attr(datetime)').get() or \
-                               response.css('div.post-subheader__meta-date time::attr(datetime)').get()
-        item['author'] = response.css('h2.contrib-byline a::text').get() or \
-                         response.css('div.post-subheader__byline a::text').get() or \
-                         response.css('div.article-info__byline a::text').get()
-        item['images'] = response.css('div.image_container img::attr(src)').getall()
+        item['body'] = ' '.join(sel.css('div.post-container p::text').getall())
 
-        # Extract NER data and add to item
+        pub_datetime_str = sel.css('div.contrib-date time::attr(datetime)').get() or \
+                           sel.css('div.post-subheader__meta-date time::attr(datetime)').get()
+        if pub_datetime_str:
+            try:
+                item['pub_datetime'] = datetime.fromisoformat(pub_datetime_str)
+            except ValueError:
+                item['pub_datetime'] = None
+        else:
+            item['pub_datetime'] = None
+
+        item['author'] = sel.css('h2.contrib-byline a::text').get() or \
+                         sel.css('div.post-subheader__byline a::text').get() or \
+                         sel.css('div.article-info__byline a::text').get()
+
+        # Update image extraction logic
+        item['images'] = sel.css('div.image_container img::attr(src)').getall()
+        item['images'] += sel.css('li.gallery__image img::attr(src)').getall()
+        item['images'] += sel.css('div.gallery__image img::attr(src)').getall()
+        item['images'] += sel.css('img::attr(src)').getall()  # Capture any remaining images
+
         body_text = clean_html(item['body'])
         item['ner'] = extract_entities(body_text)
     
@@ -59,3 +95,17 @@ class RestOfWorldSpider(scrapy.Spider):
         yield item
 
         self.article_count += 1
+
+    def scroll_to_load(self):
+        # Scroll down to the bottom to load more content
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        while True:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            self.driver.implicitly_wait(2)
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+    def closed(self, reason):
+        self.driver.quit()
